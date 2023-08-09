9c238900fb07e615e6392c29d4987e6de3a27342
dengsonghai <1443471656@qq.com>
2023-08-10 03:31:46 +0800

    设计模式更新分享

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,4 @@ public class Main {
     public static void main(String[] args) {
 
     }
-    public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
-    }
 }
